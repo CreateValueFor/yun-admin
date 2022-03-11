59274ba381d7a95c24f3271bb271fc9764cbc94e
createvaluefor <2019143077@yonsei.ac.kr>
2022-03-12 00:06:58 +0900

    initialize

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,11 +4,11 @@
 
       <Sidebar />
 
-      <div class="w-full bg-gray-100 pl-0 lg:pl-64 min-h-screen" :class="sideBarOpen ? 'overlay' : ''" id="main-content">
+      <div class="w-full flex flex-col bg-gray-100 pl-0 lg:pl-64 min-h-screen" :class="sideBarOpen ? 'overlay' : ''" id="main-content">
 
         <Navbar />
 
-        <div class="p-6 bg-gray-100 mb-20">
+        <div class="p-6 bg-gray-100 mb-20 flex-1 flex flex-col">
           <router-view />
         </div>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import Router from 'vue-router'
 
 import Dashboard from '@/components/Dashboard'
-import DashboardHome from '@/pages/Home'
+import Home from '@/pages/Home'
+import Order from '@/pages/Order'
+import Calendar from '@/pages/Calendar'
+import AddProduct from '@/pages/AddProduct'
+import ViewProduct from '@/pages/ViewProduct'
 
 import store from './store'
 
@@ -15,10 +19,15 @@ Vue.config.productionTip = false
 Vue.use(Router)
 
 const routes = [
-  { path: '/', redirect: { name: 'DashboardHome' } },
-  { path: '/dashboard', component: Dashboard, children: [
-      { path: '/', redirect: { name: 'DashboardHome' } },
-      { path: 'home', name: 'DashboardHome', component: DashboardHome }
+
+  {
+    path: '/', component: Dashboard, children: [
+      { path: '/', redirect: { name: 'Home' } },
+      { path: 'home', name: 'Home', component: Home },
+      { path: 'order', name: 'Order', component: Order },
+      { path: 'calendar', name: 'Calendar', component: Calendar },
+      { path: 'addProduct', name: 'AddProduct', component: AddProduct },
+      { path: 'manageProduct', name: 'ManageProduct', component: ViewProduct }
     ]
   }
 ]
