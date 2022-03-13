43409525b56e9f24f591b195a14a72d6ce048495
createvaluefor <2019143077@yonsei.ac.kr>
2022-03-13 17:26:54 +0900

    excel download

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -26,35 +26,92 @@
 
     <div class="flex flex-1 flex-wrap -mx-3 ">
       <div class="h-full grow w-full bg-white rounded-lg">
-        <p>주문업로드 리스트 엑셀파일을 드래그하여 옮겨주세요</p>
-        <input
-          @change="onChange"
-          type="file"
-          accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        />
+        <div v-if="!uploadedOrder.length">
+          <p>주문업로드 리스트 엑셀파일을 드래그하여 옮겨주세요</p>
+          <input
+            @change="onChange"
+            type="file"
+            accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          />
+        </div>
+        <div v-else>
+          <div class="mb-5">
+            <button
+              class="bg-green-500	rounded-lg px-6 py-2 text-white font-semibold shadow"
+              @click="downloadExcel"
+            >
+              파일 다운로드
+            </button>
+          </div>
+          <table>
+            <thead>
+              <th>구매자명</th>
+              <th>수취인명</th>
+              <th>구매자연락처</th>
+              <th>배송지</th>
+              <th>기본주소</th>
+              <th>상세주소</th>
+              <th>공동현관 비밀번호</th>
+              <th>배송메세지</th>
+              <th>상품명</th>
+              <th>시작일</th>
+              <th>종료일</th>
+              <th>단백질량</th>
+              <th>탄수화물량</th>
+              <th>탄수화물 구성</th>
+              <th>제외토핑</th>
+              <th>제외메뉴</th>
+              <th>배송</th>
+            </thead>
+            <tbody>
+              <tr v-for="(item, idx) in uploadedOrder" :key="idx">
+                <td>{{ item.구매자명 }}</td>
+                <td>{{ item.수취인명 }}</td>
+                <td>{{ item.구매자연락처 }}</td>
+                <td>{{ item.배송지 }}</td>
+                <td>{{ item['(기본주소)'] }}</td>
+                <td>{{ item['(상세주소)'] }}</td>
+                <td>{{ item['공동현관 비밀번호'] }}</td>
+                <td>{{ item['배송메세지'] }}</td>
+                <td>{{ item.상품명 }}</td>
+                <td>{{ item.시작일 }}</td>
+                <td>{{ item.종료일 }}</td>
+                <td>{{ item.단백질량 }}</td>
+                <td>{{ item.탄수화물량 }}</td>
+                <td>{{ item['탄수화물 구성'] }}</td>
+                <td>{{ item.제외토핑 }}</td>
+                <td>{{ item.제외메뉴 }}</td>
+                <td>{{ item.배송 }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-    <div>
-      <button
-        class="bg-green-500	rounded-lg px-6 py-2 text-white font-semibold shadow"
-      >
-        업로드
-      </button>
     </div>
   </div>
 </template>
 
 <script>
 import TapMenu from '../components/order/TapMenu.vue'
-import { read, utils } from 'xlsx'
+import { read, utils, writeFile } from 'xlsx'
+import custom from '@/api/custom.js'
 
 export default {
   name: 'DashboardHome',
   components: { TapMenu },
   data() {
-    return {}
+    return {
+      uploadedOrder: [],
+    }
   },
   methods: {
+    downloadExcel() {
+      const excelData = utils.json_to_sheet(this.uploadedOrder)
+      const workBook = utils.book_new()
+      utils.book_append_sheet(workBook, excelData, '주문 취합')
+      writeFile(workBook, 'test.xlsx')
+      console.log('heeloo')
+    },
     onChange(event) {
       const file = event.target.files[0]
       // const fileName = file.name
@@ -77,8 +134,9 @@ export default {
           tmpResult = roa
         })
         console.log(tmpResult)
-
-        console.log(this.ExcelDateToJSDate(tmpResult[0].결제일))
+        console.log(this.groupOrder(tmpResult))
+        this.uploadedOrder = this.processRaw(this.groupOrder(tmpResult))
+        console.log(this.processRaw(this.groupOrder(tmpResult)))
       }
       reader.readAsBinaryString(file)
     },
@@ -107,7 +165,99 @@ export default {
         seconds
       )
     },
+    groupOrder(jsonData) {
+      let init_buyer = jsonData[0].구매자명
+      let init_receiver = jsonData[0].수취인명
+      let order = []
+      let totalOrder = []
+      jsonData.forEach((item) => {
+        // 같은 주문끼리 모으기
+        if (item.구매자명 !== init_buyer || init_receiver !== item.수취인명) {
+          init_buyer = item.구매자명
+          init_receiver = item.수취인명
+          totalOrder.push(order)
+          order = []
+        }
+        order.push(item)
+      })
+      totalOrder.push(order)
+      return totalOrder
+    },
+    processRaw(groupedRaw) {
+      let initOrder = {}
+      let totalOrder = []
+      groupedRaw.forEach((order) => {
+        initOrder.구매자명 = order[0].구매자명
+        initOrder.수취인명 = order[0].수취인명
+        initOrder.구매자연락처 = order[0]['구매자연락처']
+        initOrder.배송지 = order[0]['(기본주소)'] + order[0]['(상세주소)']
+        initOrder['(기본주소)'] = order[0]['(기본주소)']
+        initOrder['(상세주소)'] = order[0]['(상세주소)']
+        // ! need to change
+        initOrder['공동현관 비밀번호'] = '없음'
+        initOrder['배송메세지'] = order[0]['배송메세지']
+        initOrder.시작일 = '설정'
+        initOrder.종료일 = '설정'
+        initOrder.단백질량 = 1
+        initOrder.탄수화물량 = 1
+        initOrder['탄수화물 구성'] = '고구마'
+        initOrder.제외토핑 = '없음'
+        order.forEach((item) => {
+          if (item.옵션정보.includes('단백질')) {
+            initOrder.단백질량 = 1.5
+          }
+          if (item.옵션정보.includes('현미밥')) {
+            initOrder['탄수화물 구성'] = item.옵션정보.split(':')[1]
+          }
+          if (item.옵션정보.includes('탄수화물')) {
+            initOrder.탄수화물량 = 1.5
+          }
+          if (item.옵션정보.includes('공동현관')) {
+            let options = item.옵션정보.split(' / ')
+            if (options.length !== 3) {
+              console.log(item)
+              // console.log(options)
+              return
+            }
+
+            const password = options[0].split(':')[1]
+            const allergy = options[1].split(':')[1]
+            const deliveryType = options[2].split(':')[1]
+            initOrder['공동현관 비밀번호'] = password
+            initOrder.제외메뉴 = allergy.trim()
+
+            initOrder.배송 = deliveryType.includes('일반') ? '일반' : '새벽'
+          }
+          if (item.옵션정보.includes('토핑')) {
+            // console.log(item.옵션정보)
+
+            initOrder.제외토핑 = item.옵션정보
+              .split('토핑')[1]
+              .replace('제외', '')
+              .trim()
+          }
+          initOrder.상품명 = custom.serviceNameFormatter(item.상품명)
+          // 공통 정보
+        })
+        totalOrder.push(initOrder)
+
+        initOrder = {}
+      })
+      return totalOrder
+    },
   },
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+table {
+  th {
+    font-size: 12px;
+  }
+  td {
+    font-size: 12px;
+    border-right: 1px solid #e2e8f0;
+    border-bottom: 1px solid #e2e8f0;
+    padding: 5px 8px;
+  }
+}
+</style>
